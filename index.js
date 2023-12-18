4f5028f1b04c6ad1a45ba3fcd4d4a779e2f87968
Mark Kelsall <99473523+geekissh@users.noreply.github.com>
2023-12-18 22:20:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  
+  ];
+  
+  function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  const winningYear = superbowlWin(record);
+  console.log(winningYear);
